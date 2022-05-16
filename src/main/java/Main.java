ae70ae521496fcccb8aeffb5a38ae6afa906a976
RomanValikov <romansbl@bk.ru>
2022-05-16 18:36:00 +0400

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("00011011", "01100110"));
+        System.out.println(binOps.mult("00011011", "01100110"));
+    }
+}
